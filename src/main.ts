f0abfc126e3f21b7d64c855eb3af413137ea6cc3
RTa-technology <scpfoundationidcardmaker@gmail.com>
2022-05-22 18:38:04 +0900

    refactor: *.ts

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,26 @@
 import css from './css/wikidot.css';
 import init from './css/init.css';
 const ftmlWorker = new Worker("/src/bundle.js");
-  // Workerスレッドから受信
-  ftmlWorker.onmessage = (event: MessageEvent) => {
-    document.querySelector("head > style#innercss")!.innerHTML=css;
-    document.querySelector("head > style#init")!.innerHTML=init;
-    const { html, styles } = event.data;
+// Workerスレッドから受信
+ftmlWorker.onmessage = (event: MessageEvent) => {
+  document.querySelector("head > style#innercss")!.innerHTML = css;
+  document.querySelector("head > style#init")!.innerHTML = init;
+  const { html, styles } = event.data;
 
-    const previewStyles = document.getElementById('preview-styles')!;
-    const previewContent = document.getElementById('preview-content')!;
-    previewContent.innerHTML = html;
-    previewStyles.innerHTML=styles.map(v=>`<style>\n${v.replace(/\\</g, '&lt;')}\n</style>`).join("\n\n");
-  };
+  const previewStyles = document.getElementById('preview-styles')!;
+  const previewContent = document.getElementById('preview-content')!;
+  previewContent.innerHTML = html;
+  previewStyles.innerHTML = styles.map(v => `<style>\n${v.replace(/\\</g, '&lt;')}\n</style>`).join("\n\n");
+};
 
-  const textareaField = document.getElementById('textarea-content')!;
+const textareaField = document.getElementById('textarea-content')!;
 
-  textareaField.addEventListener('input', (event) => {
-    const { target } = event;
-  
-    // TextArea要素以外の場合は終了
-    if (!(target instanceof HTMLTextAreaElement)) {
-      return;
-    }
-  
-    const value = target.value;
-    ftmlWorker.postMessage(value);
-  });
+textareaField.addEventListener('input', (event) => {
+  const { target } = event;
+  // TextArea要素以外の場合は終了
+  if (!(target instanceof HTMLTextAreaElement)) {
+    return;
+  }
+  const value = target.value;
+  ftmlWorker.postMessage(value);
+});
