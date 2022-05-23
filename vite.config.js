05a8b18eb5a9add9c9aa4346f8a2fdc23c778483
RTa-technology <scpfoundationidcardmaker@gmail.com>
2022-05-23 22:26:50 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -31,7 +31,8 @@ module.exports = {
     rollupOptions: {
       input: {
         main: resolve(__dirname, root, 'index.html'),
-        test: resolve(__dirname, root, 'test/index.html')
+        test: resolve(__dirname, root, 'test/index.html'),
+        test1: resolve(__dirname, root, 'test1/index.html')
       }
     },
     assetsDir: '.',
