5a1ec095b8dc918cd394f1b120ddca39d0907d6b
RTa-technology <scpfoundationidcardmaker@gmail.com>
2022-05-24 16:32:52 +0900

    fix compile

---
+++ build.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+console.time('Bundling time');
+const {build} = require('vite');
+const {join} = require('path');
+
+/** @type 'production' | 'development' | 'test' */
+const mode = process.env.MODE || 'production';
+
+const configs = [
+  join(process.cwd(), 'vite.config.js'),
+  // join(process.cwd(), 'scripts/config.browser.vite.js'),
+];
+
+
+/**
+ * Run `vite build` for config file
+ * @param {string} configFile
+ * @return {Promise<RollupOutput | RollupOutput[]>}
+ */
+const buildByConfig = (configFile) => build({configFile, mode});
+
+
+
+Promise.all(configs.map(buildByConfig))
+  .then(() => console.timeEnd('Bundling time'))
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
