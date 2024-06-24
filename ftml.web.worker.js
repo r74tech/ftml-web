06d0a66aa0ffc3dc748452422587c220ff2473b5
r74tech <r74tech@users.noreply.github.com>
2024-06-24 09:34:05 +0000

    deploy: eb41dcf5893f7d2c5a39b7f82506a66915559dce

---
+++ ftml.web.worker.js
@@ -0,0 +1,11 @@
+import { init, renderHTML, ready, loading } from './lib/ftml-wasm/esm/wj-ftml-wasm.esm.js';
+
+init();
+onmessage = async (e) => {
+  if (!ready) await loading;
+  const ftmlSource = e.data.value;
+
+  const { html, styles } = renderHTML(ftmlSource);
+  const type = e.data.type;
+  postMessage({ html, styles, type });
+};
